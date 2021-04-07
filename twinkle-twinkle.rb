e073a49889c6a326a283648399df2a0f84b6cc6a
kunal26 <sam.kapoor594@gmail.com>
2021-04-07 23:24:36 +0530

    Twinkle Twinkle little stars

---
+++ twinkle-twinkle.rb
@@ -0,0 +1,46 @@
+# Welcome to Sonic Pi
+
+#| 1 1 5 5 | 6 6 5 * | 4 4 3 3 | 2 2 1 * |
+
+#| 5 5 4 4 | 3 3 2 * | 5 5 4 4 | 3 3 2 * |
+
+use_bpm 50
+play 72
+sleep 0.5
+play 72
+sleep 0.5
+
+play 79
+sleep 0.5
+play 79
+sleep 0.5
+
+play 81
+sleep 0.5
+play 81
+sleep 0.5
+
+play 77
+sleep 1
+
+
+
+play 77
+sleep 0.5
+play 77
+sleep 0.5
+
+play 76
+sleep 0.5
+play 76
+sleep 0.5
+
+
+
+play 74
+sleep 0.5
+play 74
+sleep 0.5
+
+play 72
+sleep 0.5
